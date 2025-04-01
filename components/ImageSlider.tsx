2ebd3c49727dfcb679abea8ce9da3da0981d76b0
Jaziel96 <jazcov96@hotmail.com>
2025-04-01 17:23:54 -0600

    actualizaciones

--- components/ImageSlider.tsx
+++ components/ImageSlider.tsx
@@ -1,6 +1,7 @@
 import dynamic from 'next/dynamic';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.css';
+import { Navigation, Pagination } from 'swiper/modules';
 import Image from 'next/image';
 
 const images = [
@@ -9,29 +10,39 @@ const images = [
   '/images/logo udc.jpg',
 ];
 
-const ImageSlider = () => (
-  <Swiper
-    spaceBetween={0}
-    slidesPerView={1}
-    loop
-    pagination={{ clickable: true }}
-    navigation
-    style={{ width: '100%', height: 'auto' }} // Ajustar el ancho al 100% de la página y la altura automática
-  >
-    {images.map((src, index) => (
-      <SwiperSlide
-        key={index}
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          width: '100%', // Asegurarse de que cada slide tenga un ancho del 100%
-        }}
-      >
-        <Image src={src} alt={`Slide ${index}`} layout="responsive" width={1920} height={1080} objectFit="cover" />
-      </SwiperSlide>
-    ))}
-  </Swiper>
-);
+const ImageSlider = () => {
+  return (
+    <Swiper
+      spaceBetween={0}
+      slidesPerView={1}
+      loop
+      pagination={{ clickable: true }} // Habilitar la paginación
+      navigation // Habilitar las flechas de navegación
+      modules={[Navigation, Pagination]} // Agregar los módulos necesarios
+      style={{ width: '100%', height: '530px' }} // Ajustar el ancho al 100% de la página y la altura automática
+    >
+      {images.map((src, index) => (
+        <SwiperSlide
+          key={index}
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '100%', // Asegurarse de que cada slide tenga un ancho del 100%
+          }}
+        >
+          <Image
+            src={src}
+            alt={`Slide ${index}`}
+            layout="responsive"
+            width={1920}
+            height={1080}
+            objectFit="cover"
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
 
 export default ImageSlider;
